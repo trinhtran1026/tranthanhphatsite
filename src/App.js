7dee5bd2f7c8664a9ef4af08de742d53434c47cf
Vinh Le <vinh@vertics.co>
2019-01-29 23:08:02 +0200

    Init project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import data from './data.json';
 class App extends Component {
+
+	renderItem(item) {
+		return(
+			<div key={item.index}>
+				<h3>{item.code}</h3>
+				<img src={item.image_path} alt='product' />
+				<p>{item.category}</p>
+			</div>
+		)
+	}
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+				{
+					data.map(item => this.renderItem(item))
+				}
       </div>
     );
   }
